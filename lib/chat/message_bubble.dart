a981854fd4f87a593f65893d4c4fb0c3a20fbaa5
MichaelNZ85 <michaelnz10@outlook.com>
2020-09-03 17:15:36 +1200

    Initial commmit

---
+++ lib/chat/message_bubble.dart
@@ -0,0 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+  final bool isMe;
+  final String userName;
+  final String userImage;
+  final Key key;
+
+  MessageBubble(
+      this.message, this.userName, this.isMe, this.userImage, this.key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                ),
+              ),
+              width: 180,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 8,
+              ),
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    userName,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: isMe
+                            ? Colors.black
+                            : Theme.of(context).accentTextTheme.title.color),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: isMe
+                          ? Colors.black
+                          : Theme.of(context).accentTextTheme.title.color,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: -30,
+          left: isMe ? null : 200,
+          right: isMe ? 180 : null,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(userImage),
+          ),
+        ),
+      ],
+      overflow: Overflow.visible,
+    );
+  }
+}
